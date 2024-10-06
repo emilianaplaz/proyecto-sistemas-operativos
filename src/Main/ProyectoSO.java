29c96f416ae89715ab42402c091b38be94f9d429
emilianaplaz <emiplaz@gmail.com>
2024-10-06 12:29:31 -0400

    interfaz

--- src/Main/ProyectoSO.java
+++ src/Main/ProyectoSO.java
@@ -4,6 +4,8 @@
  */
 package Main;
 
+import GUI.Home;
+
 /**
  *
  * @author emilianaplaz
@@ -15,6 +17,8 @@ public class ProyectoSO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Home homepage = new Home ();
+        homepage.setVisible(true); 
     }
     
 }
