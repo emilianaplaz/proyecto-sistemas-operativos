fbe5d43b5bbbafc5d7dd262d2a2b4be68132154e
emilianaplaz <emiplaz@gmail.com>
2024-10-08 22:34:01 -0400

    funciones txt

--- src/Funciones/Funciones.java
+++ src/Funciones/Funciones.java
@@ -8,6 +8,7 @@ import Estructuras.ListaDoble;
 import Main.ManejadorInterfaz;
 import GUI.Global;
 import Entes.Ensamblador;
+import Entes.Productor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -22,23 +23,23 @@ public class Funciones {
     public void escribir_txt(){
         System.out.println("");
         String datos_iniciales = "";
-        //COLOCAMOS LOS DATOS DE LOS TRABAJDORES DE CARTOON NETWORK separados por coma
-        datos_iniciales = ManejadorInterfaz.getInterfaz().getAnimadoresCN()+","+
-                          ManejadorInterfaz.getInterfaz().getGuionistasCN() +","+
-                          ManejadorInterfaz.getInterfaz().getEscenariosCN()+","+
-                          ManejadorInterfaz.getInterfaz().getdoblajeCN()+","+
-                          ManejadorInterfaz.getInterfaz().getPlottwistCN()+","+
+        //Cdatos de trabajadores de hp
+        datos_iniciales = ManejadorInterfaz.getInterfaz().getProdPlacaBaseHP()+","+
+                          ManejadorInterfaz.getInterfaz().getProdCpuHP() +","+
+                          ManejadorInterfaz.getInterfaz().getProdRamHP()+","+
+                          ManejadorInterfaz.getInterfaz().getProdFuenteHP()+","+
+                          ManejadorInterfaz.getInterfaz().getProdGraficaHP()+","+
                           ManejadorInterfaz.getInterfaz().getEnsambladoresCN();
-        //COLOCAMOS UN SALTO DE LINEA Y LUEGO LOS DATOS DE LOS TRABAJDORES DE Disney separados por coma
-        datos_iniciales = datos_iniciales + "\n"+ManejadorInterfaz.getInterfaz().getAnimadoresDisney()+","+
-                                                ManejadorInterfaz.getInterfaz().getGuionistasDisney() +","+
-                                                ManejadorInterfaz.getInterfaz().getEscenariosDisney()+","+
-                                                ManejadorInterfaz.getInterfaz().getdoblajeDisney()+","+
-                                                ManejadorInterfaz.getInterfaz().getPlottwistDisney()+","+
-                                                ManejadorInterfaz.getInterfaz().getEnsambladoresDisney();
-        //COLOCAMOS LA DURACION DE LOS DIAS
+        //datos de trabajadores de apple
+        datos_iniciales = datos_iniciales + "\n"+ManejadorInterfaz.getInterfaz().getProdPlacaBaseApple()+","+
+                                                ManejadorInterfaz.getInterfaz().getProdCpuApple() +","+
+                                                ManejadorInterfaz.getInterfaz().getProdRamApple()+","+
+                                                ManejadorInterfaz.getInterfaz().getProdFuenteApple()+","+
+                                                ManejadorInterfaz.getInterfaz().getProdGraficaApple()+","+
+                                                ManejadorInterfaz.getInterfaz().getEnsambladoresApple();
+        //duracion de los dias
         datos_iniciales = datos_iniciales + "\n"+ManejadorInterfaz.getInterfaz().getDuracionDias();
-        //COLOCAMOS EL DEADLINE
+        //dias para la entrega
         datos_iniciales = datos_iniciales + "\n"+ManejadorInterfaz.getInterfaz().getDiasEntrega();
         try{
             PrintWriter pw = new PrintWriter("test\\datos_iniciales.txt");
@@ -46,7 +47,7 @@ public class Funciones {
             pw.close();
             JOptionPane.showMessageDialog(null,"Guardado exitoso");
         }catch(Exception e){
-            JOptionPane.showMessageDialog(null,"ERROR al guardar");
+            JOptionPane.showMessageDialog(null,"Error al guardar");
         }
     }
     
@@ -69,147 +70,147 @@ public class Funciones {
                 
                 if(! "".equals(txt)){
                     String[] datos_split = txt.split("\n");
-                    String[] cn = datos_split[0].split(",");
-                    String[] disney = datos_split[1].split(",");
-                    //Cargamos trabajadores de Cartoon Network
+                    String[] hp = datos_split[0].split(",");
+                    String[] apple = datos_split[1].split(",");
+                    //Cargamos trabajadores de HP
                     for (int i = 0; i < 6; i++) {
                         if(i == 0){ 
-                            for (int j = 0; j < Integer.parseInt(cn[i]); j++) {
-                                Developer animadorNuevo = new Developer(0,40,Global.getMutexCartoonNetwork(),Global.getCartoonNetwork());
+                            for (int j = 0; j < Integer.parseInt(hp[i]); j++) {
+                                Productor animadorNuevo = new Productor(0,40,Global.getMutexHP(),Global.getHP());
                                 animadorNuevo.start();
-                                Global.getCartoonNetwork().getAnimadores().insertFinal(animadorNuevo);
+                                Global.getHP().getProdPlacaBase().insertFinal(animadorNuevo);
                             }
                         }else if(i == 1){
-                            for (int j = 0; j < Integer.parseInt(cn[i]); j++) {
-                                Developer guionistaNuevo = new Developer(1,20,Global.getMutexCartoonNetwork(),Global.getCartoonNetwork());
+                            for (int j = 0; j < Integer.parseInt(hp[i]); j++) {
+                                Productor guionistaNuevo = new Productor(1,20,Global.getMutexHP(),Global.getHP());
                                 guionistaNuevo.start();
-                                Global.getCartoonNetwork().getGuionistas().insertFinal(guionistaNuevo);
+                                Global.getHP().getProdCpu().insertFinal(guionistaNuevo);
                             }
                         }else if(i == 2){
-                            for (int j = 0; j < Integer.parseInt(cn[i]); j++) {
-                                Developer DisEscenariosNuevo = new Developer(2,26,Global.getMutexCartoonNetwork(),Global.getCartoonNetwork());
+                            for (int j = 0; j < Integer.parseInt(hp[i]); j++) {
+                                Productor DisEscenariosNuevo = new Productor(2,26,Global.getMutexHP(),Global.getHP());
                                 DisEscenariosNuevo.start();
-                                Global.getCartoonNetwork().getDisenadoresEscenarios().insertFinal(DisEscenariosNuevo);
+                                Global.getHP().getProdRam().insertFinal(DisEscenariosNuevo);
                             }
                         }else if(i == 3){
-                            for (int j = 0; j < Integer.parseInt(cn[i]); j++) {
-                                Developer ActorDoblajeNuevo = new Developer(3,16,Global.getMutexCartoonNetwork(),Global.getCartoonNetwork());
+                            for (int j = 0; j < Integer.parseInt(hp[i]); j++) {
+                                Productor ActorDoblajeNuevo = new Productor(3,16,Global.getMutexHP(),Global.getHP());
                                 ActorDoblajeNuevo.start();
-                                Global.getCartoonNetwork().getActoresDoblaje().insertFinal(ActorDoblajeNuevo);
+                                Global.getHP().getProdFuentes().insertFinal(ActorDoblajeNuevo);
                             }
                         }else if(i == 4){
-                            for (int j = 0; j < Integer.parseInt(cn[i]); j++) {
-                                Developer pttNuevo = new Developer(4,34,Global.getMutexCartoonNetwork(),Global.getCartoonNetwork());
+                            for (int j = 0; j < Integer.parseInt(hp[i]); j++) {
+                                Productor pttNuevo = new Productor(4,34,Global.getMutexHP(),Global.getHP());
                                 pttNuevo.start();
-                                Global.getCartoonNetwork().getGuionistasPlotTwist().insertFinal(pttNuevo);
+                                Global.getHP().getProdGraficas().insertFinal(pttNuevo);
                             }
                         }else if(i == 5){
-                            for (int j = 0; j < Integer.parseInt(cn[i]); j++) {
-                                Assembler ensambladorNuevo = new Assembler(5,50,Global.getMutexCartoonNetwork(),Global.getCartoonNetwork());
+                            for (int j = 0; j < Integer.parseInt(hp[i]); j++) {
+                                Ensamblador ensambladorNuevo = new Ensamblador(5,50,Global.getMutexHP(),Global.getHP());
                                 ensambladorNuevo.start();
-                                Global.getCartoonNetwork().getEnsambladores().insertFinal(ensambladorNuevo);
+                                Global.getHP().getEnsambladores().insertFinal(ensambladorNuevo);
                             }
                         }
                     }
                     //actualizamos la interfaz con la cantidad de trabjadores que estan activos
-                    this.ActualizarPestanaCartoonNetwork(cn);
+                    this.ActualizarPestanaHP(hp);
                     
-                    //Cargamos trabajadores de Disney
+                    //Cargamos trabajadores de Apple
                     for (int i = 0; i < 6; i++) {
                         if(i == 0){ 
-                            for (int j = 0; j < Integer.parseInt(disney[i]); j++) {
-                                Developer animadorNuevo = new Developer(0,40,Global.getMutexDisney(),Global.getDisney());
+                            for (int j = 0; j < Integer.parseInt(apple[i]); j++) {
+                                Productor animadorNuevo = new Productor(0,40,Global.getMutexApple(),Global.getApple());
                                 animadorNuevo.start();
-                                Global.getDisney().getAnimadores().insertFinal(animadorNuevo);
+                                Global.getApple().getProdPlacaBase().insertFinal(animadorNuevo);
                             }
                         }else if(i == 1){
-                            for (int j = 0; j < Integer.parseInt(disney[i]); j++) {
-                                Developer guionistaNuevo = new Developer(1,20,Global.getMutexDisney(),Global.getDisney());
+                            for (int j = 0; j < Integer.parseInt(apple[i]); j++) {
+                                Productor guionistaNuevo = new Productor(1,20,Global.getMutexApple(),Global.getApple());
                                 guionistaNuevo.start();
-                                Global.getDisney().getGuionistas().insertFinal(guionistaNuevo);
+                                Global.getApple().getProdCpu().insertFinal(guionistaNuevo);
                             }
                         }else if(i == 2){
-                            for (int j = 0; j < Integer.parseInt(disney[i]); j++) {
-                                Developer disEscenariosNuevo = new Developer(2,26,Global.getMutexDisney(),Global.getDisney());
+                            for (int j = 0; j < Integer.parseInt(apple[i]); j++) {
+                                Productor disEscenariosNuevo = new Productor(2,26,Global.getMutexApple(),Global.getApple());
                                 disEscenariosNuevo.start();
-                                Global.getDisney().getDisenadoresEscenarios().insertFinal(disEscenariosNuevo);
+                                Global.getApple().getProdRam().insertFinal(disEscenariosNuevo);
                             }
                         }else if(i == 3){
-                            for (int j = 0; j < Integer.parseInt(disney[i]); j++) {
-                                Developer actorDoblajeNuevo = new Developer(3,16,Global.getMutexDisney(),Global.getDisney());
+                            for (int j = 0; j < Integer.parseInt(apple[i]); j++) {
+                                Productor actorDoblajeNuevo = new Productor(3,16,Global.getMutexApple(),Global.getApple());
                                 actorDoblajeNuevo.start();
-                                Global.getDisney().getActoresDoblaje().insertFinal(actorDoblajeNuevo);
+                                Global.getApple().getProdFuentes().insertFinal(actorDoblajeNuevo);
                             }
                         }else if(i == 4){
-                            for (int j = 0; j < Integer.parseInt(disney[i]); j++) {
-                                Developer pttNuevo = new Developer(4,34,Global.getMutexDisney(),Global.getDisney());
+                            for (int j = 0; j < Integer.parseInt(apple[i]); j++) {
+                                Productor pttNuevo = new Productor(4,34,Global.getMutexApple(),Global.getApple());
                                 pttNuevo.start();
-                                Global.getDisney().getGuionistasPlotTwist().insertFinal(pttNuevo);
+                                Global.getApple().getProdGraficas().insertFinal(pttNuevo);
                             }
                         }else if(i == 5){
-                            for (int j = 0; j < Integer.parseInt(disney[i]); j++) {
-                                Assembler ensambladorNuevo = new Assembler(5,50,Global.getMutexDisney(),Global.getDisney());
+                            for (int j = 0; j < Integer.parseInt(apple[i]); j++) {
+                                Ensamblador ensambladorNuevo = new Ensamblador(5,50,Global.getMutexApple(),Global.getApple());
                                 ensambladorNuevo.start();
-                                Global.getDisney().getEnsambladores().insertFinal(ensambladorNuevo);
+                                Global.getApple().getEnsambladores().insertFinal(ensambladorNuevo);
                             }
                         }
                     }
                     //actualizamos la interfaz con la cantidad de trabjadores que estan activos
-                    this.ActualizarPestanaDisney(disney);
+                    this.ActualizarPestanaApple(apple);
                     
                     //Cargamos duracion dias
-                    Global.getCartoonNetwork().setDuracionDia(Integer.parseInt(datos_split[2])*1000);
-                    Global.getDisney().setDuracionDia(Integer.parseInt(datos_split[2])*1000);
+                    Global.getHP().setDuracionDia(Integer.parseInt(datos_split[2])*1000);
+                    Global.getApple().setDuracionDia(Integer.parseInt(datos_split[2])*1000);
                     
                     //Cargamos deadline
-                    Global.getDriveCartoonNetwork().setDeadline(Integer.parseInt(datos_split[3]));
-                    Global.getDriveDisney().setDeadline(Integer.parseInt(datos_split[3]));
+                    Global.getAlmacenHP().setDeadline(Integer.parseInt(datos_split[3]));
+                    Global.getAlmacenApple().setDeadline(Integer.parseInt(datos_split[3]));
                     
                     //Cargamos el estadoDeadline
-                    Global.getDriveDisney().setEstadoDeadline(Integer.parseInt(datos_split[3]),Global.getDisney().getTipoCompania());
-                    Global.getDriveCartoonNetwork().setEstadoDeadline(Integer.parseInt(datos_split[3]), Global.getCartoonNetwork().getTipoCompania());
+                    Global.getAlmacenApple().setEstadoDeadline(Integer.parseInt(datos_split[3]),Global.getApple().getTipoCompania());
+                    Global.getAlmacenHP().setEstadoDeadline(Integer.parseInt(datos_split[3]), Global.getHP().getTipoCompania());
                     
                 }
                 br.close();
                 //JOptionPane.showMessageDialog(null,"Lectura exitosa");
             }
         }catch(Exception e){
-            JOptionPane.showMessageDialog(null,"ERROR al leer");
+            JOptionPane.showMessageDialog(null,"Error al leer");
         }
     }
     
-    public void ActualizarPestanaCartoonNetwork(String[] cn){
+    public void ActualizarPestanaHP(String[] hp){
         for (int i = 0; i < 6; i++) {
             if(i == 0){
-                ManejadorInterfaz.getInterfaz().setAnimadoresCartoonNetwork(Integer.parseInt(cn[i]));
+                ManejadorInterfaz.getInterfaz().setProdPlacaBaseHP(Integer.parseInt(hp[i]));
             }else if(i == 1){
-                ManejadorInterfaz.getInterfaz().setGuionistasCartoonNetwork(Integer.parseInt(cn[i]));
+                ManejadorInterfaz.getInterfaz().setProdCPUHP(Integer.parseInt(hp[i]));
             }else if(i == 2){
-                ManejadorInterfaz.getInterfaz().setEscenariosCartoonNetwork(Integer.parseInt(cn[i]));
+                ManejadorInterfaz.getInterfaz().setProdRamHP(Integer.parseInt(hp[i]));
             }else if(i == 3){
-                ManejadorInterfaz.getInterfaz().setDoblajesCartoonNetwork(Integer.parseInt(cn[i]));
+                ManejadorInterfaz.getInterfaz().setProdFuenteHP(Integer.parseInt(hp[i]));
             }else if(i == 4){
-                ManejadorInterfaz.getInterfaz().setGuionesPlowtTistCartoonNetwork(Integer.parseInt(cn[i]));
+                ManejadorInterfaz.getInterfaz().setProdGraficaHP(Integer.parseInt(hp[i]));
             }else if(i == 5){
-                ManejadorInterfaz.getInterfaz().setEnsambladoresCartoonNetwork(Integer.parseInt(cn[i]));
+                ManejadorInterfaz.getInterfaz().setEnsambladoresHP(Integer.parseInt(hp[i]));
             }
         }
     }
     
-    public void ActualizarPestanaDisney(String[] disney){
+    public void ActualizarPestanaApple(String[] apple){
         for (int i = 0; i < 6; i++) {
             if(i == 0){
-                ManejadorInterfaz.getInterfaz().setAnimadoresDisney(Integer.parseInt(disney[i]));
+                ManejadorInterfaz.getInterfaz().setProdPlacaBaseApple(Integer.parseInt(apple[i]));
             }else if(i == 1){
-                ManejadorInterfaz.getInterfaz().setGuionistasDisney(Integer.parseInt(disney[i]));
+                ManejadorInterfaz.getInterfaz().setProdCPUApple(Integer.parseInt(apple[i]));
             }else if(i == 2){
-                ManejadorInterfaz.getInterfaz().setEscenariosDisney(Integer.parseInt(disney[i]));
+                ManejadorInterfaz.getInterfaz().setProdRamApple(Integer.parseInt(apple[i]));
             }else if(i == 3){
-                ManejadorInterfaz.getInterfaz().setDoblajesDisney(Integer.parseInt(disney[i]));
+                ManejadorInterfaz.getInterfaz().setProdFuenteApple(Integer.parseInt(apple[i]));
             }else if(i == 4){
-                ManejadorInterfaz.getInterfaz().setGuionistasPlowtTistDisney(Integer.parseInt(disney[i]));
+                ManejadorInterfaz.getInterfaz().setProdGraficaApple(Integer.parseInt(apple[i]));
             }else if(i == 5){
-                ManejadorInterfaz.getInterfaz().setEnsambladoresDisney(Integer.parseInt(disney[i]));
+                ManejadorInterfaz.getInterfaz().setEnsambladoresApple(Integer.parseInt(apple[i]));
             }
         }
     }
